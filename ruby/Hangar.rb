0c30dae8c1ce01a0378a1c0cb4a878048e532a1a
manuel4826 <manuelgonzalezsantana15@gmail.com>
2023-03-29 17:58:09 +0200

    29-03-2023 Actualizo la clase Hangar en Ruby

--- ruby/Hangar.rb
+++ ruby/Hangar.rb
@@ -4,47 +4,68 @@ module Deepspace
   class Hangar
     def initialize(capacity)
       @maxElements=capacity
+      @shieldBoosters=Array.new()
+      @weapons=Array.new()
     end
 
     def self.newCopy(h)
-
+		new(h.maxElements);
+		h.shieldBoosters.each{addShieldBooster(h.shieldBoosters.at(i)}
+		h.weapons.each{addWeapon(h.weapons.at(i))}
     end
 
     def getUIversion
+		return HangarToUI.new(self)
     end
 
     private
     def spaceAvailable
-
+		(@shieldBoosters.length + @weapons.length) < @maxElements
     end
 
     public
     def addWeapon(w)
-
+		insertar = true;
+		if(spaceAvailable) @weapons.push(w)
+		else insertar = false 
+		return insertar
     end
 
     def addShieldBooster(s)
-
+		insertar = true
+		if(spaceAvailable) @shieldBoosters.push(s)
+		else insertar = false
+		return insertar
     end
 
     def maxElements
-
+		@maxElements
     end
 
-    def shieldBooster
-
+    def shieldBoosters
+		@shieldBoosters
     end
 
     def weapons
-
+		@weapons
     end
 
-    def removeShieldBooster(i)
-
+    def removeShieldBooster(s)
+		shield = nil
+		if (0<=w && w<@weapons.length) 
+			shield = @shieldBoosters.at(s)
+			@shieldBoosters.delete_at(s)
+		end
+		return weapon
     end
 
-    def removeWeapon
-
+    def removeWeapon(w)
+		weapon = nil
+		if (0<=w && w<@weapons.length) 
+			weapon = @weapons.at(w)
+			@weapons.delete_at(w)
+		end
+		return weapon
     end
 
     def to_s
