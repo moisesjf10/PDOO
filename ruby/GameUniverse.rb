c39cd9fe2f9f7d9f94a66856bbcbefc58f0a23f7
Moisés Jiménez Fernández <moisesjf10@gmail.com>
2023-04-21 10:30:24 +0200

    21/04

--- ruby/GameUniverse.rb
+++ ruby/GameUniverse.rb
@@ -15,12 +15,55 @@ module Deepspace
     end
 
     def combatGo(station, enemy)
+      enemyWins = false
+      ch = @dice.firstShot
+      combatResult = CombatResult::ENEMYWINS
 
+      if ch == GameCharacter::ENEMYSTARSHIP
+        fire = enemy.fire
+        result = station.receiveShot(fire)
+
+        if result == ShotResult::RESIST
+          fire = station.fire
+          result = enemy.receiveShot(fire)
+          enemyWins = (result == ShotResult::RESIST)
+        else
+          enemyWins = true
+        end
+      else
+        fire = station.fire
+        result = enemy.receiveShot(fire)
+        enemyWins = (result == ShotResult::RESIST)
+      end
+
+      if enemyWins
+        s = station.getSpeed
+        moves = @dice.spaceStationMoves(s)
+
+        if moves
+          damage = enemy.damage
+          station.setPendingDamage(damage)
+          combatResult = CombatResult::ENEMYWINS
+        else
+          station.move
+          combatResult = CombatResult::STATIONESCAPES
+        end
+      else
+        aloot = enemy.loot
+        station.setLoot(aloot)
+        combatResult = CombatResult::STATIONWINS
+      end
+      return combatResult
     end
 
     public
     def combat()
-
+      result = CombatResult::NOCOMBAT
+      state = @gameState.state
+      if state == GameState::BEFORECOMBAT || state == GameState::INIT
+        result = combatGo(@currentStation,@currentEnemy)
+      end
+      return result
     end
 
     def discardHangar
@@ -69,7 +112,7 @@ module Deepspace
         state = @gameState.state
         dice = Dice.new()
         	if(state == GameState::CANNOTPLAY)
-        		dealer = CardDealer.new()
+        		dealer = CardDealer.instance
         		names.each do |n|
         		    supplies = dealer.nextSuppliesPackage()
         		    station = SpaceStation.new(n,supplies)
@@ -103,7 +146,22 @@ module Deepspace
     end
 
     def nextTurn
-        state = @gameState.state
+      turn = false
+      state = @gameState.state
+      if state == GameState::AFTERCOMBAT
+        stationState = @currentStation.validState
+        if stationState
+          @currentStationIndex = (@currentStationIndex+1)%@spaceStations.size()
+          @turns+=1
+          @currentStation = @spaceStations.at(@currentStationIndex)
+          @currentStation.cleanUpMountedItems
+          CardDealer dealer = CardDealer.instance
+          @currentEnemy = dealer.nextEnemy
+          @gameState.next(@turns,@spaceStations)
+          turn = true
+        end
+      end
+      return turn
     end
 
     def to_s
